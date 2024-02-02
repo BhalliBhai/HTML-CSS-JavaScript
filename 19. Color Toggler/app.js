b14d36a75728f59bdc159bfbe671cf3b709ede60
Bhalli Bhai <94342592+BhalliBhai@users.noreply.github.com>
2024-02-03 04:18:47 +0500

    Color Toggler Update

--- 19. Color Toggler/app.js
+++ 19. Color Toggler/app.js
@@ -1,13 +1,9 @@
 let toggler = document.getElementById("switch");
 
 toggler.addEventListener("click", () => {
-  //   if (toggler.checked === true) {
-  //     document.body.style.backgroundColor = "black";
-  //   } else {
-  //     document.body.style.backgroundColor = "white";
-  //   }
 
   toggler.checked === true
     ? (document.body.style.backgroundColor = "black")
     : (document.body.style.backgroundColor = "white");
+  
 });
