934b90077f55b70435e419a3888c51c7efea1764
Bhalli Bhai <94342592+BhalliBhai@users.noreply.github.com>
2024-02-16 01:30:46 +0500

    Removed C logs

--- 22. Fade Out/app.js
+++ 22. Fade Out/app.js
@@ -2,7 +2,6 @@ let arrow = document.getElementById("arrow");
 
 window.addEventListener("scroll", () => {
   let position = window.scrollY;
-  // console.log(position);
   if (position <= 5) {
     arrow.classList.toggle("fade-in");
     arrow.classList.toggle("fade-out");
